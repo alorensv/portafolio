c23dd62cb601818c58d8d63217bca61e90e63ec4
Alorensv <alorensv@gmail.com>
2025-08-14 18:20:03 -0400

    Portafolio nuevos problemas en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
+  ],
   server: {
     port: 4321,
     host: true
